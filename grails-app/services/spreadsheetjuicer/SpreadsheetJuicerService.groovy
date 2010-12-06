7d401a4a482feec741e78a9ee9d0891b261aadef
aaron <aaron@aaron-desktop.(none)>
2010-12-06 16:49:15 -0600

    updated service methods

--- grails-app/services/spreadsheetjuicer/SpreadsheetJuicerService.groovy
+++ grails-app/services/spreadsheetjuicer/SpreadsheetJuicerService.groovy
@@ -1,4 +1,4 @@
-package spreadsheetJuicer
+package com.gvl
 
 import org.apache.poi.ss.usermodel.*
 import org.apache.poi.hssf.usermodel.*
@@ -36,13 +36,13 @@ class SpreadsheetJuicerService {
     def cleanSsData
     switch(uploadFile.contentType) {
       case "application/vnd.ms-excel":
-        cleanSsData = xlsToSimpleHash(uploadFile, columTypes, ignoredRows)
+        cleanSsData = xlsToSimpleHash(uploadFile.inputStream, columTypes, ignoredRows)
         break
       case "application/vnd.oasis.opendocument.spreadsheet":
-        cleanSsData = odsToSimpleHash(uploadFile, columTypes, ignoredRows)
+        cleanSsData = odsToSimpleHash(uploadFile.inputStream, columTypes, ignoredRows)
         break
       case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-        cleanSsData = xlsxToSimpleHash(uploadFile, columTypes, ignoredRows)
+        cleanSsData = xlsxToSimpleHash(uploadFile.inputStream, columTypes, ignoredRows)
         break
       default:
         println "not valid file type?"
@@ -54,13 +54,13 @@ class SpreadsheetJuicerService {
     def cleanSsData
     switch(uploadFile.contentType) {
       case "application/vnd.ms-excel":
-        cleanSsData = excelToHash(uploadFile, columTypes, ignoredRows)
+        cleanSsData = excelToHash(uploadFile.inputStream, columTypes, ignoredRows)
         break
       case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-        cleanSsData = excelToHash(uploadFile, columTypes, ignoredRows)
+        cleanSsData = excelToHash(uploadFile.inputStream, columTypes, ignoredRows)
         break
       case "application/vnd.oasis.opendocument.spreadsheet":
-        cleanSsData = odsToHash(uploadFile, columTypes, ignoredRows)
+        cleanSsData = odsToHash(uploadFile.inputStream, columTypes, ignoredRows)
         break
       default:
         println "not valid file type?"
@@ -68,24 +68,29 @@ class SpreadsheetJuicerService {
     return cleanSsData
   }
   
-//    Idealy we want the resulting hash to be in this format
+//    Idealy we want the resulting hash to be in this format -- not necessarily JSON syntax. 
 //      var documents = [
-//          { //repeats - represents a sheet
-//          metadata: {
-//            columns: Column_Count,
-//            rows: Row_Count,
-//            title: ''
-//          },
-//          data: {
-//            r{Row_Index}: { //repeats
-//              c{Column_Index}: '' //repeats
+//        { //repeats
+//            metadata: {
+//                columns: Column_Count,
+//                rows: Row_Count,
+//                title: ''
+//            },
+//            data: {
+//                r{Row_Index}: { //repeats
+//                    c{Column_Index}: { //repeats
+//                        value: '',
+//                        style: '',
+//                        width: 0,
+//                        cl: {Classes used for styling}
+//                    }
+//                }
 //            }
-//          }
 //        }
-//      ];
+//      ]
   
-  def xlsToSimpleHash(uploadFile, columTypes, ignoredRows) {
-    Workbook wb = new HSSFWorkbook(uploadFile.inputStream)
+  def xlsToSimpleHash(inputStream, columTypes, ignoredRows) {
+    Workbook wb = new HSSFWorkbook(inputStream)
     //so this is just going to get sheet 1 .. who uses more than 1?
     //def numOfSheets = wb.getNumberOfSheets() 
     Sheet sheet = wb.getSheetAt(0)
@@ -108,7 +113,7 @@ class SpreadsheetJuicerService {
             default:
               xlsData[rowNum][colNum] = ''
           }
-          //println xlsData[rowNum][colNum] + " - class: " xlsData[rowNum][colNum].class
+          //println xlsData[rowNum][colNum] + " - class: "xlsData[rowNum][colNum].class
         }
       }
       else println "ignoring row ${rowNum}"
@@ -116,12 +121,12 @@ class SpreadsheetJuicerService {
     return xlsData
   }
   
-  def xlsxToSimpleHash(uploadFile, columTypes, ignoredRows) {
-    //def wb = new XSSFWorkbook(uploadFile.inputStream)
+  def xlsxToSimpleHash(inputStream, columTypes, ignoredRows) {
+    //def wb = new XSSFWorkbook(inputStream)
   }
   
-  def odsToSimpleHash(uploadFile, columTypes, ignoredRows) {
-    ODPackage odp = new ODPackage(uploadFile.inputStream)
+  def odsToSimpleHash(inputStream, columTypes, ignoredRows) {
+    ODPackage odp = new ODPackage(inputStream)
     SpreadSheet ods = SpreadSheet.create(odp)
     //def sheetCount = ss.getSheetCount() 
     def sheet = ods.getSheet(0)
@@ -143,14 +148,14 @@ class SpreadsheetJuicerService {
     return odsData
   }
   
-  def xlsToHash(uploadFile, colTypes, ignoredRows) {
-    Workbook wb = new HSSFWorkbook(uploadFile.inputStream)
+  def xlsToHash(inputStream, colTypes, ignoredRows) {
+    Workbook wb = new HSSFWorkbook(inputStream)
     //so this is just going to get sheet 1 .. who uses more than 1?
     //def numOfSheets = wb.getNumberOfSheets() 
     def xlsData = []
     Sheet sheet = wb.getSheetAt(0)
     def rowCount = sheet.getLastRowNum()+1
-      def colCount = sheet.getRow(0).getLastCellNum()
+    def colCount = sheet.getRow(0).getLastCellNum()
     println "rows: "+ rowCount + " -- columns: " + colCount
     xlsData[0] = ["metadata":["columns":colCount, "rows":rowCount, "title":sheet.getSheetName()], "data":[:]]
     sheet.iterator().eachWithIndex{row, rowNum ->
@@ -158,17 +163,17 @@ class SpreadsheetJuicerService {
       row.iterator().eachWithIndex{cell, colNum ->
         // get cellType returns 1 for string 0 for numeric (and 2 for formula?)
         if(cell.getCellType()){
-          try{xlsData[0]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = cell.getStringCellValue()}
+          try{xlsData[0]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = ["value":cell.getStringCellValue(),"style":"","width":0,"cl":[]]}
           catch(Throwable e){println e}
         }
         else{
           // excel stores dates as a formatted Double. check the format to see if it is a date cell
           if( new DateUtil().isCellDateFormatted(cell) ){
-            try{xlsData[0]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = cell.getDateCellValue()}
+            try{xlsData[0]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = ["value":cell.getDateCellValue(),"style":"","width":0,"cl":[]]}
             catch(Throwable e){println e}
           }
           else {
-            try{xlsData[0]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = cell.getNumericCellValue()}
+            try{xlsData[0]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = ["value":cell.getNumericCellValue(),"style":"","width":0,"cl":[]]}
             catch(Throwable e){println e}
           }
         } 
@@ -177,41 +182,56 @@ class SpreadsheetJuicerService {
     return xlsData
   }
   
-  def excelToHash(uploadFile, colTypes, ignoredRows) {
-    def wb = new WorkbookFactory().create(uploadFile.inputStream)
+  def excelToHash(inputStream, colTypes, ignoredRows) {
+    def wb = new WorkbookFactory().create(inputStream)
     def excelData = []
     def numOfSheets = wb.getNumberOfSheets()-1
     (0..numOfSheets).each{sheetNum ->
       def sheet = wb.getSheetAt(sheetNum)
       //the sheet needs to have at least one row... not sure what is best detector...
       //maybe if(sheet.getPhysicalNumberOfRows()>0 || sheet.getFirstRowNum())
-      // println "physical rows: "+ sheet.getPhysicalNumberOfRows()
+      //println "physical rows: "+ sheet.getPhysicalNumberOfRows()
       def firstRow = sheet.getFirstRowNum()
       if(sheet.getRow(firstRow)){
-        def rowCount = sheet.getLastRowNum()
+        def rowCount = sheet.getLastRowNum()+1
         def colCount = sheet.getRow(firstRow).getLastCellNum()
-        println "rows: "+ rowCount + " -- columns: " + colCount
+        //println "rows: "+ rowCount + " -- columns: " + colCount
         excelData[sheetNum] = [
-          "metadata":["columns":colCount, "rows":rowCount, "title":sheet.getSheetName()],
+          "metadata":["columns":colCount, "rows":rowCount,"title":sheet.getSheetName()],
           "data":[:]
         ]
         sheet.rowIterator().eachWithIndex{row, rowNum ->
-          excelData[sheetNum]["data"]['r'+(rowNum+1)] = [:]
+          excelData[sheetNum]["data"]['r'+(rowNum)] = [:]
           row.cellIterator().eachWithIndex{cell, colNum ->
-            if(cell.getCellType()){
-              try{excelData[sheetNum]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = cell.getStringCellValue()}
-              catch(Throwable e){println e}
+            def kindOfCell = cell.getCellType() // 0 = numeric, 1 = String, 2 = Formula, 3 = Blank, 4 = Boolean, 5 = Error
+            def cellVal
+            switch(kindOfCell) {
+              case 0:
+                cellVal = cell.getNumericCellValue()
+                break
+              case 1:
+                cellVal = cell.getStringCellValue()
+                break
+              case 2:
+                cellVal = cell.getCachedFormulaResultType()
+                break
+              case 3:
+                cellVal = ''
+                break
+              case 4:
+                cellVal = cell.getBooleanCellValue()
+                break
+              default:
+                cellVal = "error"
+                println "cell evaluation error"
             }
-            else{
-              // excel stores dates as a formatted Double. check the format to see if it is a date cell
-              if( new DateUtil().isCellDateFormatted(cell) ){
-                try{excelData[sheetNum]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = cell.getDateCellValue()}
-                catch(Throwable e){println e}
-              }
-              else {
-                try{excelData[sheetNum]["data"]['r'+(rowNum+1)]['c'+(colNum+1)] = cell.getNumericCellValue()}
-                catch(Throwable e){println e}
-              }
+            try{
+              excelData[sheetNum]["data"]['r'+(rowNum)]['c'+(colNum)] = ["value":"${cellVal}",
+                                                                         "style":"",
+                                                                         "width":0,
+                                                                         "cl":["cell"]]
+            } catch(Throwable e) {
+              println 'sheet:'+sheetNum+' -- r:'+rowNum+' -- c:'+colNum+' -- err:'+e
             }
           }
         }
@@ -220,8 +240,8 @@ class SpreadsheetJuicerService {
     return excelData
   }
   
-  def odsToHash(uploadFile, colTypes, ignoredRows) {
-    ODPackage odp = new ODPackage(uploadFile.inputStream)
+  def odsToHash(inputStream, colTypes, ignoredRows) {
+    ODPackage odp = new ODPackage(inputStream)
     SpreadSheet ods = SpreadSheet.create(odp)
     //def sheetCount = ss.getSheetCount() 
     def sheet = ods.getSheet(0)
@@ -232,7 +252,8 @@ class SpreadsheetJuicerService {
     def colCount = colTypes.size()
     println "rows: "+ rowCount + " -- columns: " + colCount
     def odsData = []
-    odsData[0] = ["metadata":["columns":colCount, "rows":rowCount, "title":1], "data":[:]]
+    odsData[0] = ["metadata":["columns":colCount, "rows":rowCount, "title":1],
+"data":[:]]
     (1..rowCount).each{rowNum ->
       odsData[0]["data"]['r'+rowNum] = [:]
       (1..colCount).each{colNum ->
